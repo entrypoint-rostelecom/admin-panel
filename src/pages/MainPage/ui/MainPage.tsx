8d8548b39ac0dbd0ea2efd8661f56f333139b158
nickolaykrasilov <krasilov.kolya@yandex.ru>
2026-03-22 11:01:53 +0300

    style: fix dark theme

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -4,12 +4,22 @@ import { Page } from "@/widgets/Page";
 import { clearAccessToken, useAdminLogoutMutation, useUserActions } from "@/entities/User";
 import { getRouteLogin, getRouteRegister, getRouteUsers } from "@/shared/consts/router";
 import { useAppSelector } from "@/shared/lib/hooks";
+import { ThemeSwitcher } from "@/features/ThemeSwitcher";
+import { LanguageSwitcher } from "@/shared/ui/LanguageSwitcher";
+import { useTranslation } from "react-i18next";
 import classes from "./MainPage.module.css";
 
 const MainPage = memo(() => {
 	const user = useAppSelector((state) => state.user.authData);
 	const { clearAuthData } = useUserActions();
 	const [adminLogout] = useAdminLogoutMutation();
+	const { t, i18n } = useTranslation();
+
+	const themeType = useAppSelector((state) => state.ui.themeStyles.type);
+
+	const currentLang = (i18n.language || "ru").split("-")[0];
+
+	const logoSrc = `/assets/Image/RGB_RT_logo-vertical_${themeType === "dark" ? "black" : "main"}_${currentLang}.png`;
 
 	const onLogout = () => {
 		adminLogout(undefined)
@@ -23,7 +33,25 @@ const MainPage = memo(() => {
 
 	return (
 		<Page>
+			<div className={classes.floatingControls}>
+				<ThemeSwitcher />
+				<LanguageSwitcher />
+			</div>
+			
 			<div className={classes.root}>
+				<div className={classes.logoContainer}>
+					<img
+						src={logoSrc}
+						alt={t("common.brand")}
+						className={classes.logo}
+						onLoad={() => console.log("LOGO SUCCESS: Loaded from", logoSrc)}
+						onError={(e) => {
+							console.error("LOGO ERROR: Failed to load", logoSrc);
+							console.log("Current document.baseURI is", document.baseURI);
+						}}
+					/>
+				</div>
+
 				<h1 className={classes.title}>Главная страница</h1>
 
 				{user ? (
